38237b289ff7b7f2beda62ea2c5a3b0051f86f05
unknown <sanchez_tw@hotmail.com>
2022-04-08 13:53:27 -0300

    correccion commit 25

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -119,11 +119,6 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num % 2) == 0;
-  return true;
-  }
-
-  return false
   
 }
 
