fad28d2d6c2c883ee5dad2f12762bca6a163a793
unknown <sanchez_tw@hotmail.com>
2022-04-12 20:48:43 -0300

    commit nombrescombinados

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -208,6 +208,9 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
+  var nombre = str1;
+  var apellido = str2;
+  return (' str1' +' str2');
   
 }
 
