d8ce26eb8495cb7be5794a2631a8e2d931b40381
unknown <sanchez_tw@hotmail.com>
2022-04-12 15:21:43 -0300

    commit positivo2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -189,10 +189,10 @@ function esPositivo(numero) {
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
   if (numero > 0) {
-    return "es positivo";
+    return "Es positivo";
   }
     if (numero < 0) {
-      return "es negativo";
+      return "Es negativo";
     }
     return false;
 }
