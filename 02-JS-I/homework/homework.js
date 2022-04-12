f94d88c679fc9379c0085573a227a7a27fe8ff81
unknown <sanchez_tw@hotmail.com>
2022-04-12 15:47:46 -0300

    commit exclamacion 4

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -201,7 +201,7 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  console.log(str + '!');
+  return (str + '!');
 }
 
 function combinarNombres(nombre, apellido) {
