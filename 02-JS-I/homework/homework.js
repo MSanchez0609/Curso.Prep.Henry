91c2e192d330e4c6b1f3533a7afda8d356fb32dc
unknown <sanchez_tw@hotmail.com>
2022-04-20 11:57:09 -0300

    commit vocal 02

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -260,6 +260,9 @@ function esVocal(letra){
   if (letra = "a", "e", "i", "o", "u") {
       return "Es vocal";
     }
+    if (letra !== "a"| "e"| "i"| "o"| "u") {
+      return "Dato incorrecto";
+    }
   }
   
 
